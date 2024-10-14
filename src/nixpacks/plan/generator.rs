392ed8b48b83541912de969d4d7a23642b06e8ee
marianoguerra <marianoguerra@users.noreply.github.com>
2024-10-14 17:06:20 +0000

    Update nixpkgs archive to `b69de56fac8c2b6f8fd27f2eca01dcda8e0a4221`
    Signed-off-by: GitHub <noreply@github.com>

--- src/nixpacks/plan/generator.rs
+++ src/nixpacks/plan/generator.rs
@@ -7,9 +7,9 @@ use anyhow::{Context, Ok, Result};
 use std::collections::HashMap;
 
 // This line is automatically updated.
-// Last Modified: 2022-08-22 17:06:26 UTC+0000
-// https://github.com/NixOS/nixpkgs/commit/54060e816971276da05970a983487a25810c38a7
-pub const NIXPKGS_ARCHIVE: &str = "54060e816971276da05970a983487a25810c38a7";
+// Last Modified: 2024-10-14 17:06:19 UTC+0000
+// https://github.com/NixOS/nixpkgs/commit/b69de56fac8c2b6f8fd27f2eca01dcda8e0a4221
+const NIXPKGS_ARCHIVE: &str = "b69de56fac8c2b6f8fd27f2eca01dcda8e0a4221";
 
 #[derive(Clone, Default, Debug)]
 pub struct GeneratePlanOptions {
